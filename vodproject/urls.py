d72d01864ba15e32c0d07b89a11f2c6e36d88de1
recordhyo <gytla1@naver.com>
2023-11-07 09:11:35 +0900

    social로그인 + email인증

--- vodproject/urls.py
+++ vodproject/urls.py
@@ -4,8 +4,9 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("vod/", include("contents.urls")),
-    path("accounts/", include("accounts.urls")),
     path("admin/", admin.site.urls),
+    path('api/users/',include('allauth.urls')),
+    path("contents/", include("contents.urls")),
+    path("accounts/", include("accounts.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home")
 ]
